653ab4cc25204ec390d62517870060b76e9eaa8b
Pipeto84 <pipejarauribeboteroa@gmail.com>
2026-02-26 14:49:17 -0500

    productos en array

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,7 +1,51 @@
 import '../styles/Productos.css';
 
+const productos = [
+  {
+    id: 'P1',
+    name: 'Radiador Jeep Wrangler',
+    price: 600000,
+  },
+  {
+    id: 'P2',
+    name: 'Radiador Honda Niche High Capacity',
+    price: 239649,
+  },
+  {
+    id: 'P3',
+    name: 'Radiador Champion Cooling Systems MC6062 All Aluminum 1960',
+    price: 1162196,
+  },
+  {
+    id: 'P4',
+    name: 'Radiador 26.06" Kia Spectra',
+    price: 285048,
+  },
+  {
+    id: 'P5',
+    name: 'Radiador Spectra Premium cu2458',
+    price: 531584,
+  },
+  {
+    id: 'P6',
+    name: 'Radiador Hose Bottom Right',
+    price: 288628
+  },
+  {
+    id: 'P7',
+    name: 'Radiador Engine Water Coolant Pipe Hose',
+    price: 55322
+  },
+  {
+    id: 'P8',
+    name: 'Radiador Bracket Cushion Seat ',
+    price: 48000
+  }
+]
 export const Productos = () => {
   return (
-    <div className="productos">Productos</div>
+    <div className="productos">
+      <h1>Productos</h1>
+    </div>
   )
 }
