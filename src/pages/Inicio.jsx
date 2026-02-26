3550577a4afe9cd7332b2452a97fcc4966a21a20
Pipeto84 <pipejarauribeboteroa@gmail.com>
2026-02-26 17:36:15 -0500

    cambio inicio

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,11 +1,21 @@
-import logo from '../assets/logo.jpeg'
+import logo from "../assets/logo.jpeg";
+import inicio1 from "../assets/inicio/inicio1.jpg";
+import inicio2 from "../assets/inicio/inicio2.jpg";
+import inicio3 from "../assets/inicio/inicio3.jpg";
+import inicio4 from "../assets/inicio/inicio4.jpg";
 import "../styles/Inicio.css";
 
 export const Inicio = () => {
   return (
     <div className="inicio">
-      <h1 className='infoInicio'>Bienvenidos a Radiadores S&T</h1>
-      <img src={logo} className="logo" alt="logo" />
+      <div className="contInicio">
+        <img src={inicio1} className="imagenInicio" alt="inicio1" />
+        <h2 className="infoInicio">Bienvenidos a<hr/> Radiadores S&T</h2>
+        <img src={inicio4} className="imagenInicio" alt="inicio4" />
+        <img src={inicio2} className="imagenInicio" alt="inicio2" />
+        <img src={logo} className="logo" alt="logo" />
+        <img src={inicio3} className="imagenInicio" alt="inicio3" />
+      </div>
     </div>
   );
 };
