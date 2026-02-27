9ddcf9265a34f743b95e1c5df66046b774a76777
Pipeto84 <pipejarauribeboteroa@gmail.com>
2026-02-26 19:48:44 -0500

    mapa de navegacion listo

--- src/pages/Mapa.jsx
+++ src/pages/Mapa.jsx
@@ -1,7 +1,29 @@
-import '../styles/Mapa.css'
+import logo from "../assets/logo.jpeg";
+import { NavLink } from "react-router-dom";
+import "../styles/Mapa.css";
 
 export const Mapa = () => {
   return (
-    <div className="mapa">Mapa</div>
-  )
-}
+    <div className="mapa">
+      <img src={logo} className="logo" alt="logo" />
+      <p className="tituloMapa">Mapa de navegación</p>
+      <ul className="lista">
+        <li>
+          <NavLink to="/" className="nav-link-mapa">
+            Inicio
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/productos" className="nav-link-mapa">
+            Productos
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/nosotros" className="nav-link-mapa">
+            Nosotros
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
